2cbbd83a0bd60aa24990044333764fdf1a16f18d
James Kolsby <jrkolsby@mac.com>
2017-07-03 21:43:41 -0400

    Updated about page

--- about/index.php
+++ about/index.php
@@ -15,18 +15,17 @@
         <a id="community" href="../community/">Community</a>
         <a id="equipment" href="../equipment">Equipment</a>
     </nav>
-    <h1 id="title">Information Desk</h1>
+    <h1 id="title">About Us</h1>
     <section id="about">
-        <h1>Lorem ipsum.</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum odit rem perferendis quidem quaerat, sunt ab, fuga doloremque quae necessitatibus!</p>
-        <h2>Lorem</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quisquam, ad architecto quibusdam laborum modi laudantium dicta provident maxime aliquam, quasi laboriosam? Hic porro repudiandae consequuntur voluptates. Tempore voluptas consectetur expedita omnis. Deserunt optio quod exercitationem nostrum molestiae suscipit quaerat quas dolorem voluptatum facilis, ex labore excepturi, porro, dolores repudiandae nobis culpa earum. Tempora vel sunt error voluptates blanditiis quo non enim, doloribus animi, placeat a voluptatum. Saepe architecto, unde sed fuga. Accusantium incidunt excepturi veniam harum nam, doloremque, dolorum magnam. Ducimus laborum placeat, dolorum numquam itaque nam dolorem incidunt ab, molestias atque perferendis dignissimos molestiae minus voluptatem ad excepturi.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ea incidunt maxime quisquam natus commodi voluptatum labore dolor in asperiores aliquam voluptates, vel necessitatibus, quod quam quo. Totam ut rem velit alias earum, culpa temporibus sit assumenda possimus at deserunt veritatis repellat, sunt doloremque esse, expedita eum quisquam ex aperiam!</p>
+        
+        <p class="missionstatement">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum odit rem perferendis quidem quaerat, sunt ab, fuga doloremque quae necessitatibus!</p>
+        <h1>Getting Involved</h1>
+        <h1>Board Members</h1>
         <h2>Lorem ipsum dolor sit amet.</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet expedita laborum, similique voluptates! Nisi similique corrupti quod consectetur numquam necessitatibus!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, quos!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore minima maxime explicabo sint optio culpa, quasi? Nemo laborum assumenda sequi quas aliquam ab nesciunt cumque facere accusantium eos, ad vero similique vel debitis repellat sunt porro quam cupiditate, veritatis qui quasi mollitia dolor pariatur ut numquam. Voluptatem nesciunt, ipsam nihil libero a. Totam molestiae repudiandae autem quaerat dignissimos voluptatum sed sit, at quae molestias! Quas asperiores animi pariatur ad ipsam voluptatibus similique in aspernatur omnis soluta, quia a error corrupti maiores quaerat, rem, atque nisi eaque. Voluptatibus illo fuga nulla nemo eaque vitae harum! Doloribus, accusantium ut! Nam soluta earum, eum ratione, quo dolores culpa sint qui veritatis id expedita ab itaque quasi consequuntur voluptas magnam reprehenderit illum molestiae repellat nisi odit debitis. Aliquid, itaque quo. Minus corporis sit, explicabo recusandae, officia eum aliquam animi. Facere, sint consequatur hic quaerat, ex porro, fugit a consectetur harum rem neque quam quas.</p>
-        <h1>Lorem.</h1>
+        <h1>Contact</h1>
+        <p>Facebook link</p>
+        <h2>Board Members</h2>
+        <p></p>
         </section>
 </body>
 </html>
